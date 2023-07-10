8f56c884c71f3869f356aa773fcada97cce03f2a
Supul Kalhara <supul9229kalhara@gmail.com>
2023-07-11 00:47:46 +0530

    add-initial-components

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import ThemeProvider from '@mui/material/styles/ThemeProvider';
+import Routes from './pages/routes';
+
+function App() {
+    return (
+        <div className="App">
+            <Routes/>
+        </div>
+    );
+
+}
+
+export default App;
