e304cbb626e8c7322bad58ec3d006e72bd7ff464
Supul Kalhara <supul9229kalhara@gmail.com>
2023-07-11 11:19:23 +0530

    add all components with dummy data

--- src/pages/routes.jsx
+++ src/pages/routes.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Dashboard from "./layout/Dashboard";
 import BookingContainer from "./create-booking/BookingContainer";
+import ViewBookingsContainer from './view-bookings/ViewBookingsContainer';
+import AddNewResourceContainer from './add-resource/AddNewResourceContainer';
+import ViewResourcesContainer from './view-resources/ViewResourcesContainer';
 
 const AppRoutes = () => {
 
@@ -9,7 +12,10 @@ const AppRoutes = () => {
         <BrowserRouter basename={process.env.PUBLIC_URL}>
             <Routes>
                 <Route path="/" element={<Dashboard/>}/>
-                <Route path="/booking" element={<BookingContainer/>}/>
+                <Route path="/book" element={<BookingContainer/>}/>
+                <Route path="/bookings" element={<ViewBookingsContainer/>}/>
+                <Route path="/addresource" element={<AddNewResourceContainer/>}/>
+                <Route path="/viewresources" element={<ViewResourcesContainer/>}/>
             </Routes>
         </BrowserRouter>
     );
