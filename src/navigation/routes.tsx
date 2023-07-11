b7b6e2ef1e6a0ba9f5a6e5ed586c120e148d460f
Danusha <danush99online@gmail.com>
2023-07-12 04:24:48 +0530

    get routes into the navigation directory

---
+++ src/navigation/routes.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Dashboard from "../pages/layout/Dashboard";
+import BookingContainer from "src/pages/create-booking/BookingContainer";
+import ViewBookingsContainer from 'src/pages/view-bookings/ViewBookingsContainer';
+import AddNewResourceContainer from 'src/pages/add-resource/AddNewResourceContainer';
+import ViewResourcesContainer from 'src/pages/view-resources/ViewResourcesContainer';
+
+const AppRoutes = () => {
+
+    return (
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
+            <Routes>
+                <Route path="/" element={<Dashboard/>}/>
+                <Route path="/book" element={<BookingContainer/>}/>
+                <Route path="/bookings" element={<ViewBookingsContainer/>}/>
+                <Route path="/addresource" element={<AddNewResourceContainer/>}/>
+                <Route path="/viewresources" element={<ViewResourcesContainer/>}/>
+            </Routes>
+        </BrowserRouter>
+    );
+};
+
+export default AppRoutes;
