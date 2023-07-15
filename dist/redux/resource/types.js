592b79baba9fcd842c80eadfa934dd95bdd589f6
Nethum Lamahewage <nethumpinsara@gmail.com>
2023-07-15 18:35:41 +0530

    Remove dist from repository

--- dist/redux/resource/types.js
+++
@@ -1,3 +0,0 @@
-export const RESOURCE_ENQUEUE = 'RESOURCE_ENQUEUE';
-export const RESOURCE_REMOVE = 'RESOURCE_REMOVE';
-//# sourceMappingURL=types.js.map
--- dist/services/HttpServices.js
+++
@@ -1,4 +0,0 @@
-import axios from 'axios';
-export const baseURL = import.meta.env.VITE_BASE_URL;
-export default axios.create({ baseURL: baseURL });
-//# sourceMappingURL=HttpServices.js.map
