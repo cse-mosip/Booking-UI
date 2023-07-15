22173ed2721bfca2dbe3d259ff1e4fdee1a0ddf5
Nethum Lamahewage <nethumpinsara@gmail.com>
2023-07-15 19:21:13 +0530

    Properly configure TypeScript

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import envCompatible from 'vite-plugin-env-compatible';
+import svgrPlugin from 'vite-plugin-svgr';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  envPrefix: 'REACT_APP_',
+
+  plugins: [react(), envCompatible(), tsconfigPaths(), svgrPlugin()],
+  resolve: {
+    alias: {
+      src: '/src',
+    },
+  },
+  build: {
+    outDir: 'build',
+  },
+  server: {
+    port: 3000,
+    open: true,
+  },
+});
