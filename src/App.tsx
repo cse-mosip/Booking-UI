e3b2c8f33be4df23a624199de0cd6c05ace00514
Nethum Lamahewage <nethumpinsara@gmail.com>
2023-07-16 13:36:00 +0530

    Temporarily log backend URL in frontend

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import {ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
+    console.log(import.meta.env.REACT_APP_BACKEND_URL);
     return (
         <div className="App">
             <Provider store={store}>
